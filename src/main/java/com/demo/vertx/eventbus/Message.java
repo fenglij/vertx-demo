dc54653dbf12fcd9d2c779c1565b5f7c4014c821
fenglij <feng_lj_89@163.com>
2018-09-05 13:59:19 +0800

    EventBus 消息解码器

--- src/main/java/com/demo/vertx/eventbus/Message.java
+++ src/main/java/com/demo/vertx/eventbus/Message.java
@@ -1,4 +1,22 @@
 package com.demo.vertx.eventbus;
 
 public class Message {
+    private String type;
+    private Object msg;
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public Object getMsg() {
+        return msg;
+    }
+
+    public void setMsg(Object msg) {
+        this.msg = msg;
+    }
 }
